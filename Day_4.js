d307ee54c48ed5f8e14ec64482b20021c294ba64
Inamul07 <mohammedinamul87@gmail.com>
2021-05-20 20:30:58 +0530

    Day_4

--- Day_4.js
+++ Day_4.js
@@ -1,3 +1,9 @@
+/*Declare a constant variable,(PI), and assign it the value Math.PI. 
+You will not pass this challenge unless the variable is declared as a constant and named PI (uppercase).
+Read a number,(r), denoting the radius of a circle from stdin.
+Use (r) and (PI) to calculate the (area) and (perimeter) of a circle having radius .
+Print (area) as the first line of output and print (perimeter) as the second line of output. */
+
 'use strict';
 
 process.stdin.resume();
