1833c0714e7ddeff744c727e4bcfdee65d371198
Inamul07 <mohammedinamul87@gmail.com>
2021-06-03 18:05:50 +0530

    Day_18

---
+++ Day_18.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Day_18 {
+    public static void main(String[] args) {
+        ArrayList<Integer> List = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            int val = sc.nextInt();
+            List.add(val);
+        }
+        for(int i = 0; i < List.size(); i++){
+            List.set(i, List.get(i)*List.get(i));
+        }
+        System.out.print(List);
+        sc.close();
+    }
+}
