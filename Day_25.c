c6bb52c71b54c4b93380cd2a8535016a6a55e4d8
Inamul07 <mohammedinamul87@gmail.com>
2021-06-10 22:53:44 +0530

    Day_25

---
+++ Day_25.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void sort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++){
+        for (j = 0; j < n - i - 1; j++){
+            if (arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n], count = 0;
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    for (int i = 0; i < n; i++){
+        if (arr[i] % 2 == 0)
+            arr[count++] = arr[i];
+    }
+    arr[count] = '\0';
+    sort(arr, count);
+    if(arr[1] % 2 != 0 || arr[1] == 0)
+        printf("Your Data Inputs Does Not Contain Enough Even Numbers");
+    else
+        printf("Second Smallest Even Number is = %d",arr[1]);
+}
