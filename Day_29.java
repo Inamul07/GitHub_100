304a09aa59033a5c818b2f20e5cefc131e4a4319
Inamul07 <mohammedinamul87@gmail.com>
2021-06-14 23:06:37 +0530

    Day_29

---
+++ Day_29.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Day_29 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            int val = sc.nextInt();
+            list.add(val);
+        }
+        for(int i = 0; i < n; i++)
+            if(list.get(i) % 2 == 1)
+                System.out.print(list.get(i));
+        sc.close();
+    }
+}
