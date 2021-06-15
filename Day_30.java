87ed83ec40fc5a478b5ac852391dece8c0e57beb
Inamul07 <mohammedinamul87@gmail.com>
2021-06-15 14:03:12 +0530

    Day_30

---
+++ Day_30.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Day_30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The No. Of Terms:");
+        int n = sc.nextInt();
+        int f_term = 0, s_term = 1;
+        System.out.println(f_term + "\n" + s_term);
+        for(int i = 2; i < n; i++) {
+            int temp = s_term;
+            s_term = f_term + s_term;
+            f_term = temp;
+            System.out.println(s_term);
+        }
+        sc.close();
+    }
+}
