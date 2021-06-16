08a1018ddb9cae85b84d0c725b183b80920fbdbc
Inamul07 <mohammedinamul87@gmail.com>
2021-06-16 08:26:20 +0530

    Day_31

---
+++ Day_31.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Day_31 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter No. Of Elements: ");
+        int n = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        System.out.print("Enter " + n + " Elements:\n");
+        for(int i = 0; i < n; i++){
+            int val = sc.nextInt();
+            list.add(val);
+        }
+        int sum = 0;
+        for(int i = 0; i < n; i++)
+            sum += list.get(i);
+        System.out.print("Sum = " + sum);
+        sc.close();
+    }
+}
