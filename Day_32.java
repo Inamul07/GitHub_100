e7ccd22b168cda983e88a0e0384772ca600e7ba5
Inamul07 <mohammedinamul87@gmail.com>
2021-06-17 21:13:09 +0530

    Day_32

---
+++ Day_32.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Day_32 {
+    static String strrev(String str) {
+        int len = str.length();
+        String rev = "";
+        for(int i = len-1; i >= 0; i--)
+            rev += str.charAt(i);
+        return rev;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = sc.nextLine();
+        String rev = strrev(str);
+        System.out.print("Reverse of the String is "+rev);
+        sc.close();
+    }
+}
