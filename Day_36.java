37a5482f7153672b36eb8d52a517483f67a40530
Inamul07 <mohammedinamul87@gmail.com>
2021-06-21 14:32:01 +0530

    Day_36

---
+++ Day_36.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Day_36 {
+    static boolean isprime(int n) {
+        int temp = 0;
+        for(int i = 2; i < n; i++) {
+            if(n % 2 == 0){
+                temp = 1;
+                break;
+            }
+        }
+        if(temp == 0)
+            return true;
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), count = 0, val = 1;
+        while(true) {
+            if(isprime(val)) {
+                System.out.print(val + " ");
+                count++;
+            }
+            if(count == n)
+                break;
+            val++;
+        }
+        sc.close();
+    }
+}
