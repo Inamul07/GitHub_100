b1b65650736646b36f5267e4a1ef540a33485ff5
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2021-07-10 23:29:25 +0530

    Day_30

--- fibonacci.java
+++ fibonacci.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Day_30 {
+public class fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter The No. Of Terms:");
