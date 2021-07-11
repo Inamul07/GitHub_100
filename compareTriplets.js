17e92f2eeba97cbc587247ba57394f52092cc953
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2021-07-11 22:48:39 +0530

    Day_8

--- compareTriplets.js
+++ compareTriplets.js
@@ -1,26 +1,3 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
 
 /*
  * Complete the 'compareTriplets' function below.
