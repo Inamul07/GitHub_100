12093c52ae6b2076d97b4190d7d9a3e2ae109912
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2021-07-12 22:16:15 +0530

    Day_56

---
+++ diagnolDifference.c
@@ -0,0 +1,68 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+int diagonalDifference(int arr_rows, int arr_columns, int** arr) {
+    int rdiag = 0, ldiag = 0;
+    for(int i=0;i<arr_rows;i++){
+        for(int j=0;j<arr_columns;j++){
+            if(i == j){
+                ldiag += arr[i][j];
+            }
+        }
+        rdiag += arr[i][arr_columns-1-i];
+    }
+    return abs(ldiag-rdiag);
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int n = parse_int(ltrim(rtrim(readline())));
+
+    int** arr = malloc(n * sizeof(int*));
+
+    for (int i = 0; i < n; i++) {
+        *(arr + i) = malloc(n * (sizeof(int)));
+
+        char** arr_item_temp = split_string(rtrim(readline()));
+
+        for (int j = 0; j < n; j++) {
+            int arr_item = parse_int(*(arr_item_temp + j));
+
+            *(*(arr + i) + j) = arr_item;
+        }
+    }
+
+    int result = diagonalDifference(n, n, arr);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+
