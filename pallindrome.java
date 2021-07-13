f971f4c76e5629e4bb1f0da5cac430c9b5a1c990
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2021-07-13 18:18:17 +0530

    Day_40

--- pallindrome.java
+++ pallindrome.java
@@ -1,5 +1,6 @@
+/* Write a java program to find if a givven string is pallindrome or not */
 import java.util.*;
-public class Day_40 {
+public class pallindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
