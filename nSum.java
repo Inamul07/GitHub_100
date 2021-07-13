1de3b175cac6d37b0173a2c5f09fbce1f937537a
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2021-07-13 18:10:28 +0530

    Day_37

--- nSum.java
+++ nSum.java
@@ -1,6 +1,6 @@
 /* Write a java program to calculate sum of n numbers [1 to n] */
 import java.util.*;
-public class Day_37 {
+public class nSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), sum = 0;
