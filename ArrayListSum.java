e33eec73b36ff8b832f0a2f0e2bcf262641e764f
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2021-08-22 23:14:44 +0530

    Day_31

--- ArrayListSum.java
+++ ArrayListSum.java
@@ -1,7 +1,7 @@
 /* Get n elements of a list and return the sum of the list */
 
 import java.util.*;
-public class arrayListSum {
+public class ArrayListSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter No. Of Elements: ");
