8e901d3968062012907b82cf4ebfd770bf90fcd5
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2021-09-06 22:34:01 +0530

    Day_35

--- Prime.java
+++ Prime.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class prime {
+public class Prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), temp = 0;
