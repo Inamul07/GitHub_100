6c8570c60e167f3192a5b210f23e35390a918f8a
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2021-10-03 16:19:25 +0530

    Day_94

---
+++ ContainsDuplicate.java
@@ -0,0 +1,13 @@
+// url: https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i: nums) {
+            if(set.contains(i))
+                return true;
+            set.add(i);
+        }
+        return false;
+    }
+}
