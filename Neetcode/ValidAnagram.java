382d946bdb6aa4aad011da549fd2ff64d0fd2aff
Inamul07 <mohammedinamul87@gmail.com>
2022-07-13 20:28:46 +0530

    Neetcode Day 1

--- Neetcode/ValidAnagram.java
+++ Neetcode/ValidAnagram.java
@@ -24,3 +24,18 @@ class Solution {
         // return true;
     }
 }
+
+// Solution 2:
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alpha = new int[26];
+        for(char ch: s.toCharArray()) alpha[ch - 'a']++;
+        for(char ch: t.toCharArray()) alpha[ch - 'a']--;
+        
+        for(int i = 0; i < 26; i++) {
+            if(alpha[i] != 0) return false;
+        }
+        return true;        
+    }
+}
