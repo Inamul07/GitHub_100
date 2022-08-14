1fca2cbf552dd6e0858399d21ff2eb33285a96bd
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2022-08-14 22:17:56 +0530

    leetcode 258

---
+++ Leetcode/AddDigits.java
@@ -0,0 +1,8 @@
+// url: https://leetcode.com/problems/add-digits/
+
+class Solution {
+    public int addDigits(int num) {
+        if(num == 0) return 0;
+        return num%9==0? 9: num%9;
+    }
+}
