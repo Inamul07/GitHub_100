a3c4c7ada363dc71db3f8a9a84814290351fa326
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2022-08-25 22:10:56 +0530

    leetcode 383

---
+++ Leetcode/ransomNote.java
@@ -0,0 +1,14 @@
+// url: https://leetcode.com/problems/ransom-note/
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] alpha = new int[26];
+        for(char ch: ransomNote.toCharArray()) alpha[ch - 'a']++;
+        for(char ch: magazine.toCharArray()) {
+            if(alpha[ch - 'a'] > 0) alpha[ch - 'a']--;
+        }
+        for(int i = 0; i < 26; i++)
+            if(alpha[i] != 0) return false;
+        return true;
+    }
+}
