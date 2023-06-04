103951557cd8924f6a60dbc86a94b532183a3ac9
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2023-06-04 12:20:01 +0530

    Leetcode 547

--- Leetcode/NumberOfProvinces.java
+++ Leetcode/NumberOfProvinces.java
@@ -1,5 +1,46 @@
 // url: https://leetcode.com/problems/number-of-provinces/
 
+// Breadth First Search
+class Solution {
+
+    Map<Integer, List<Integer>> map;
+    Set<Integer> visited;
+    int provinces = 0;
+
+    private void bfs(int node) {
+        if(visited.contains(node)) return;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
+        visited.add(node);
+        provinces++;
+        while(!q.isEmpty()) {
+            int n = q.remove();
+            for(int child: map.get(n)) {
+                if(visited.contains(child)) continue;
+                q.add(child);
+                visited.add(child);
+            }
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        map = new HashMap<>();
+        visited = new HashSet<>();
+        for(int i = 0; i < isConnected.length; i++) {
+            map.putIfAbsent(i, new ArrayList<>());
+            for(int j = 0; j < isConnected[0].length; j++) {
+                if(i == j) continue;
+                if(isConnected[i][j] == 1) {
+                    map.get(i).add(j);
+                }
+            }
+        }
+        for(int i: map.keySet()) bfs(i);
+        return provinces;
+    }
+}
+
+// Union Find
 class Solution {
 
     int[] parent, rank;
