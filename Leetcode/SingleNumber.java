a5428ba48304d31a2cf5a6ab7116838d7ab061f2
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2023-07-04 09:30:08 +0530

    Leetcode 136

---
+++ Leetcode/SingleNumber.java
@@ -0,0 +1,9 @@
+// url: https://leetcode.com/problems/single-number/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for(int i: nums) res ^= i;
+        return res;
+    }
+}
