055a4230fbeaf9feb7f9a28ee705be083a44c0fc
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2023-08-06 10:15:25 +0530

    Leetcode 96

---
+++ Leetcode/UniqueBinarySearchTrees.java
@@ -0,0 +1,11 @@
+// url: https://leetcode.com/problems/unique-binary-search-trees/
+
+class Solution {
+    public int numTrees(int n) {
+        if(n == 1) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 1; dp[1] = 1;
+        for(int i = 2; i <= n; i++) for(int j = 0; j < i; j++) dp[i] += dp[j] * dp[i-1-j];
+        return dp[n];
+    }
+}
