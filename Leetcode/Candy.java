59f5cbda62980337d56e46029d81c77b3b6ae868
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2023-09-13 20:14:23 +0530

    Leetcode 135

---
+++ Leetcode/Candy.java
@@ -0,0 +1,16 @@
+// url: https://leetcode.com/problems/candy/
+
+class Solution {
+    public int candy(int[] ratings) {
+        if(ratings.length == 1) return 1;
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+        for(int i = 1; i < ratings.length; i++) 
+            if(ratings[i] > ratings[i-1]) candies[i] = candies[i-1] + 1;
+        for(int i = ratings.length-2; i >= 0; i--) 
+            if(ratings[i] > ratings[i+1] && candies[i] <= candies[i+1]) candies[i] = candies[i+1] + 1;
+        int res = 0;
+        for(int i: candies) res += i;
+        return res;
+    }
+}
