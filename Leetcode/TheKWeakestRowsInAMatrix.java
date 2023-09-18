cb8f09af678518a385e1632fc2a4f5eb90579ed4
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2023-09-18 11:20:31 +0530

    Leetcode 1337

--- Leetcode/TheKWeakestRowsInAMatrix.java
+++ Leetcode/TheKWeakestRowsInAMatrix.java
@@ -1,36 +1,28 @@
 // url: https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
 
-class Pair {
-
-    int n1, n2;
-
-    Pair(int n1, int n2) {
-        this.n1 = n1;
-        this.n2 = n2;
-    }
-
-    @Override
-    public String toString() {
-        return n1 + "=" + n2 + ", ";
-    }
-}
-
 class Solution {
-    public int[] kWeakestRows(int[][] mat, int k) {
-        List<Pair> list = new ArrayList<>();
-        int res[] = new int[k], idx = 0;
-        for(int i = 0; i < mat.length; i++) {
-            int count = 0;
-            for(int j = 0; j < mat[0].length; j++) if(mat[i][j] == 1) count++;
-            list.add(new Pair(i, count));
+
+    private int findSoldierCount(int[] nums) {
+        if(nums[0] == 0) return 0;
+        if(nums[nums.length-1] == 1) return nums.length;
+        int start = 0, end = nums.length-1;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(mid > 0 && nums[mid] == 0 && nums[mid-1] == 1) return mid;
+            if(nums[mid] == 1) start = mid+1;
+            else if(nums[mid] == 0) end = mid-1;
         }
-        Collections.sort(list, new Comparator<Pair>() {
-            @Override 
-            public int compare(Pair p1, Pair p2) {
-                return Integer.compare(p1.n2, p2.n2);
-            }
+        return start;
+    }
+
+    public int[] kWeakestRows(int[][] mat, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            if(a[1] == b[1]) return a[0]-b[0];
+            return a[1]-b[1];
         });
-        for(int i = 0; i < k; i++) res[idx++] = list.get(i).n1;
+        for(int i = 0; i < mat.length; i++) pq.add(new int[] {i, findSoldierCount(mat[i])});
+        int[] res = new int[k];
+        for(int i = 0; i < k; i++) res[i] = pq.remove()[0];
         return res;
     }
 }
