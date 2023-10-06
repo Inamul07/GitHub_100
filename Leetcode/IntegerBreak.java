4ebd7041529a075d85d4a49979bf1b2711c7346f
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2023-10-06 19:57:08 +0530

    Leetcode 343

--- Leetcode/IntegerBreak.java
+++ Leetcode/IntegerBreak.java
@@ -1,5 +1,6 @@
 // url: https://leetcode.com/problems/integer-break/
 
+// Recursive Solution
 class Solution {
 
     Map<Integer, Integer> map;
@@ -19,5 +20,22 @@ class Solution {
     public int integerBreak(int n) {
         map = new HashMap<>();
         return dfs(n, n);
+    }
+}
+
+
+// DP Solution
+class Solution {
+    public int integerBreak(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        for(int num = 2; num < n+1; num++) {
+            dp[num] = (num == n)? 0: num;
+            for(int i = 1; i < num; i++) {
+                int val = dp[i] * dp[num - i];
+                dp[num] = Math.max(dp[num], val);
+            }
+        }
+        return dp[n];
     }
 }
