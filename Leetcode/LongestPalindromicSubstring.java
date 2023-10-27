353efdcb8045d681ccc4b80e247a15a82d9a3f92
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2023-10-27 19:36:36 +0530

    Leetcode 5

--- Leetcode/LongestPalindromicSubstring.java
+++ Leetcode/LongestPalindromicSubstring.java
@@ -2,27 +2,26 @@
 
 class Solution {
 
-    String res;
-    int maxLen;
+    int leftPtr, maxLen;
 
     private void longestPalFromCenter(String s, int l, int r) {
         while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
             if(r - l + 1 > maxLen) {
-                res = s.substring(l, r+1);
+                leftPtr = l;
                 maxLen = r - l + 1;
             }
-            l --;
+            l--;
             r++;
         }
     }
 
     public String longestPalindrome(String s) {
-        res = "";
         maxLen = 0;
+        leftPtr = 0;
         for(int i = 0; i < s.length(); i++) {
             longestPalFromCenter(s, i, i);
             longestPalFromCenter(s, i, i+1);
         }
-        return res;
+        return s.substring(leftPtr, leftPtr + maxLen);
     }
 }
