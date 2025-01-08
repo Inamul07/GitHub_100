264d771350cbcbb7e6a881e100cf4cfb9e26a046
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2025-01-08 22:00:06 +0530

    LC Day 8

---
+++ Leetcode_100/8_CountPrefixAndSuffixPairsI.java
@@ -0,0 +1,12 @@
+// url: https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/
+class Solution {
+    public int countPrefixSuffixPairs(String[] words) {
+        int res = 0;
+        for(int i = 0; i < words.length; i++) {
+            for(int j = i+1; j < words.length; j++) {
+                if(words[j].startsWith(words[i]) && words[j].endsWith(words[i])) res++;
+            }
+        }
+        return res;
+    }
+}
