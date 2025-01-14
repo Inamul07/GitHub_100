f4bfc999125e171e98f002b83aacbbea37aa0d78
Mohammed Inamul Hassan <77063164+Inamul07@users.noreply.github.com>
2025-01-14 10:46:42 +0530

    LC Day 14

---
+++ Leetcode_100/14_MinimumLengthOfStringAfterOperations.java
@@ -0,0 +1,13 @@
+// url: https://leetcode.com/problems/minimum-length-of-string-after-operations/
+class Solution {
+    public int minimumLength(String s) {
+        int[] alpha = new int[26];
+        int len = 0;
+        for(int i = 0; i < s.length(); i++) alpha[s.charAt(i)-'a']++;
+        for(int i = 0; i < 26; i++) {
+            if(alpha[i] != 0 && alpha[i]%2 == 0) len += 2;
+            else if(alpha[i] != 0) len += 1;
+        }
+        return len;
+    }
+}
